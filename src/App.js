1ba2c37cd87e449a14b18995865838bf42a3a563
Alan Uecker <alan.uecker@img.ly>
2020-02-19 09:15:09 +0100

    fixed font

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 
 import { UIEvent, PhotoEditorSDKUI } from "photoeditorsdk";
-import { ExampleLibraryProvider } from "./provider";
 
 class App extends Component {
   /** If this config is changed the it will always override the control gui settings */
@@ -11,11 +10,9 @@ class App extends Component {
     engine: {
       license: ""
     },
-    assetBaseUrl: "http://imgly.github.io/pesdk-html5/assets",
     library: {
       enableUpload: true,
       enableWebcam: true,
-      provider: ExampleLibraryProvider
     }
   };
 
@@ -28,48 +25,6 @@ class App extends Component {
     this.editor.export(false).then(image => {
       console.log(image);
     });
-  };
-
-  serialize = options => {
-    this.editor.serialize(options).then(serialized => {
-      if (options.mode === "download") {
-        var dataStr =
-          "data:text/json;charset=utf-8," +
-          encodeURIComponent(JSON.stringify(serialized, true, 2));
-        var link = document.createElement("a");
-        link.setAttribute("href", dataStr);
-        link.setAttribute("download", "serialization-3.8.0.json");
-        link.click();
-      } else if (options.mode === "log") {
-        console.log(JSON.stringify(serialized, true, 2));
-      }
-    });
-  };
-
-  deserialize = () => {
-    const fileInput = document.createElement("input");
-    fileInput.type = "file";
-    fileInput.accept = "application/JSON";
-    fileInput.addEventListener("change", () => {
-      if (fileInput.files == null || !fileInput.files.length) {
-        return;
-      }
-      const file = fileInput.files[0];
-      const reader = new FileReader();
-      reader.onload = () => {
-        const data = JSON.parse(reader.result);
-        this.editor
-          .deserialize(data)
-          .then(() => {
-            console.log("Finished deserialization");
-          })
-          .catch(error => {
-            console.error("errror", error);
-          });
-      };
-      reader.readAsText(file);
-    });
-    fileInput.click();
   };
 
   init() {
